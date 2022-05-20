c50c10a4b851ba8b021a5f24b0a205bcdf969add
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2022-05-20 23:53:21 +0300

    runs properly

---
+++ tureng.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+word = sys.argv[1]
+url = "https://tureng.com/en/turkish-english/"+word
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+try:
+    print(soup.find("h2").text)
+except:
+    print("no result")
+    exit()
+soup  = soup.find("table",{"class":"table table-hover table-striped searchResultsTable"}).text.split("\n")
+result = []
+
+for item in soup:
+    if len(item) > 0:
+        result.append(item)
+
+
+result.pop(0)
+result.pop(0)
+result.pop(0)
+
+for i in range (len(result)):
+    print(result[i])
